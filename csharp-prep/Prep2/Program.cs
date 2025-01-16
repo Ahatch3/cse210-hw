364d63acec8de7ecebe8473abfc0198f56010f70
Ahatch3 <austinkhatch@gmail.com>
2025-01-15 19:39:49 -0700

    finished this part of the homework

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        string nNumber;
+
+        string grade;
+
+        Console.WriteLine("What is your percentage number?");
+
+        nNumber = Console.ReadLine();
+
+        float number = int.Parse(nNumber);
+
+        if (number >= 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine(grade);
     }
 }
