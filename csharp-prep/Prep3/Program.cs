de3e5c15a00514a4b45ddf78c2b88068a1c890c9
Ahatch3 <austinkhatch@gmail.com>
2025-01-25 16:31:07 -0700

    idk

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rng = new Random();
+
+        int m_number = rng.Next(101);
+
+        string response;
+
+        Console.WriteLine("What is the magic number? ");
+
+        response = Console.ReadLine();
+
+        int m_response = int.Parse(response);
+
+        if (m_response == m_number)
+        {
+            Console.WriteLine("Congrats you got it!");
+        }
+        else if (m_response < m_number)
+        {
+            Console.WriteLine("You're a little low... Guess higher!");
+        }
+        else
+        {
+            Console.WriteLine("You're a little high... Guess lower! ");
+        }
+
+    
+
+        while (m_response != m_number)
+        {
+
+            Console.WriteLine("What is the magic number? ");
+
+            response = Console.ReadLine();
+
+            m_response = int.Parse(response);
+
+            
+
+            if (m_response == m_number)
+            {
+                Console.WriteLine("Congrats you got it!");
+            }
+            else if (m_response < m_number)
+            {
+                Console.WriteLine("You're a little low... Guess higher!");
+            }
+            else
+            {
+                Console.WriteLine("You're a little high... Guess lower! ");
+            }
+        }
+
+
+
+
     }
 }
