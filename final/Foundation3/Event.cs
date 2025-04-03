6b6c28a41bff9d640b773cccf47c6b8ef55ef9c9
Ahatch3 <austinkhatch@gmail.com>
2025-04-03 17:56:15 -0600

    initializing foundation 3

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,14 @@
+
+
+class Event{
+
+    protected string _eventTitle;
+
+    protected string _description;
+
+    protected int _date;
+
+    protected int _time;
+
+    protected string _address;
+}
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation3 World!");
+
+        int menu = 0;
+
+
+        while (menu != 4){
+
+            Console.WriteLine("-- Welcome to the Event Planner --\n\n1. Make an Event\n2. View Event\n3. Setup Events for Demo\n4. Quit");
+
+
+            if(menu == 1){
+
+
+            }
+            if (menu == 2){
+
+            }
+
+            if (menu == 3){
+
+
+            }
+
+
+        }
+
+
     }
 }
